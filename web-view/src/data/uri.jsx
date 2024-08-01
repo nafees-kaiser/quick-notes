7412d180858ae9936b8baf539b9aae11314aa86a
Syed Nafees Kaiser <oyonafees2001@gmail.com>
2024-08-01 13:26:02 +0600

    Change db connection url

--- web-view/src/data/uri.jsx
+++ web-view/src/data/uri.jsx
@@ -1,1 +1,2 @@
-export const BASE_URI = "http://localhost:8080/api/v1";
+// export const BASE_URI = "http://localhost:8080/api/v1";
+export const BASE_URI = "https://quick-notes-qxqg.onrender.com/api/v1"
